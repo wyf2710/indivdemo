25a15891c8db5eacddd492c570a47e179c55d276
wyf <11382001+lsp20220503@user.noreply.gitee.com>
2023-03-12 23:54:49 +0800

    改动1

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h2>这是一个关于网页--wyf</h2>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <h1>我来了</h1>
   </div>
 </template>
 
